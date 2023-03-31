d9cba2745055bcd84e6e544dbb33f898dffa0892
hashLahiru <hasinthalahiru333@gmail.com>
2023-04-01 00:51:12 +0530

    Appointments Page Created

---
+++ client/src/pages/Appointments.js
@@ -0,0 +1,78 @@
+import React, { useEffect, useState } from "react";
+import { useDispatch } from 'react-redux';
+import Layout from "../components/Layout";
+import { showLoading, hideLoading } from "../redux/alertsSlice";
+import { toast } from "react-hot-toast";
+import axios from "axios";
+import { Table } from "antd";
+import moment from 'moment';
+
+function Appointments() {
+    const [appointments, setAppointments] = useState([]);
+    const dispatch = useDispatch();
+    const getAppointmentsData = async () => {
+        try {
+            dispatch(showLoading());
+            const response = await axios.get("/api/user/get-appointments-by-user-id", {
+                headers: {
+                    Authorization: `Bearer ${localStorage.getItem("token")}`,
+                },
+            });
+            dispatch(hideLoading());
+            if (response.data.success) {
+                setAppointments(response.data.data);
+            }
+        } catch (error) {
+            dispatch(hideLoading());
+        }
+    };
+    const columns = [
+        {
+            title: "Id",
+            dataIndex: "_id",
+        },
+        {
+            title: "Officer",
+            dataIndex: "name",
+            render: (text, record) => (
+                <span>
+                    {record.officerInfo.firstName} {record.officerInfo.lastName}
+                </span>
+            ),
+        },
+        {
+            title: "Email",
+            dataIndex: "email",
+            render: (text, record) => (
+                <span>
+                    {record.officerInfo.email}
+                </span>
+            ),
+        },
+        {
+            title: "Date & Time",
+            dataIndex: "createdAt",
+            render: (text, record) => (
+                <span>
+                    {moment(record.date).format("DD-MM-YYYY")} {moment(record.time).format("HH:mm")}
+                </span>
+            ),
+        },
+        {
+            title: "Status",
+            dataIndex: "status",
+        }
+    ];
+    
+    useEffect(() => {
+        getAppointmentsData();
+    }, []);
+
+    return <Layout>
+        <h1 className="page-title">Appointments</h1>
+        <hr />
+        <Table columns={columns} dataSource={appointments} />
+    </Layout>
+}
+
+export default Appointments;
